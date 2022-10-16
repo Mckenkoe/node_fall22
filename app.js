2f9351eab6fe221a879dbbe0a8c5485edaa12ce5
Mckenkoe <koetitz_mckenna@columbusstate.edu>
2022-10-16 23:41:05 +0000

    connected database

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const { response } = require('express');
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
-app.use('/static',express.static("public"));
-app.set("view engine", "ejs");
+const mongoDB = 'mongodb+srv://mckenko:mrk8019122@cluster0.av1xhkk.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoDB)
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console,"MongoDB connection error"));
 
-app.get('/hello', function(req,res){
-    res.send('Hello World');
-})
+app.use('/static',express.static("public"));
+app.use(express.urlencoded({ extended: true}))
+app.set("view engine", "ejs");
 
 app.get('/', function(req,res){
     res.render('todo.ejs');
+})
+
+app.post('/',(req, res) =>{
+    console.log(req.body.content)
 })
 
 app.listen(3000, function(){
